dc4a5358affd6a06660e1830e4f655b5a0e56733
yrtc99 <yrtc99@gmail.com>
2023-07-11 20:24:45 +0800

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Sidebar, Navbar } from './components';
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
+
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
